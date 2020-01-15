aef5bc6d17c1972be7252acfde53d03e4ac198b9
MDZX5 <34048272+MDZX5@users.noreply.github.com>
2020-01-15 00:41:09 -0500

    Add project files.

---
+++ MDZFBLACommunityService/AdminHub.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MDZFBLACommunityService
+{
+    /// <summary>
+    /// Interaction logic for AdminHub.xaml
+    /// </summary>
+    public partial class AdminHub : Window
+    {
+        public AdminHub()
+        {
+            InitializeComponent();
+            
+        }
+
+
+
+
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+
+        private void Asd_Click(object sender, RoutedEventArgs e)
+        {
+            Page1 x = new Page1();
+            MainFrame.Navigate(x);
+        }
+
+        private void MainFrame_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+
+        }
+    }
+}
